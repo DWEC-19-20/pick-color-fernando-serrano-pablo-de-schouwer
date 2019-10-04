cc4881509cd752380b9eafd884bf796f43ae354b
Pablo De Schouwer <deschouwer.pablo@gmail.com>
2019-10-04 09:19:56 +0200

    testeando

--- js/rgb.js
+++ js/rgb.js
@@ -1,23 +1,5 @@
 'use strict';
 
-/* Función que al pasar un valor RGB devuelva el resultado en representación hexadecimal
-
-
-
-
-function rgb2hex(rgb){
-  rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
-  return (rgb && rgb.length === 4) ? "#" +
-   ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
-   ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
-   ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
- }
- 
- 
- $('button').click(function(){
-     var hex = rgb2hex( $('input').val() );
-     $('.result').html( hex );
- });*/
 
  
     var r = document.getElementById("r");

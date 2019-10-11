d4b42222484c71ed7c50c42d2826d77287b97af5
Pablo De Schouwer <deschouwer.pablo@gmail.com>
2019-10-11 09:32:00 +0200

    fin

--- js/rgb.js
+++ js/rgb.js
@@ -4,7 +4,10 @@ var r=document.getElementById('r');
 var g=document.getElementById('g');
 var b=document.getElementById('b');
 
-var hex=document.getElementById('hex');
+var hexadecimal=document.getElementById('hexadecimal');
+
+
+
 
 function convert(){
 
@@ -12,36 +15,26 @@ function convert(){
   var green= g.value;
   var blue= b.value;
 
-  var hexC= "#"+getHex(red)+getHex(green)+getHex(blue);
-  hex.value=hexC;
+  var hexC= "#"+convertAHexa(red)+convertAHexa(green)+convertAHexa(blue);
+  hexadecimal.value=hexC;
 };
-function getHex(value){
-  var hexa=parseInt(value).toString(16);
-  return hexa.length==1?"0"+hexa:hexa;
-
-};
-/* Función que al pasar un valor RGB en representación hexadecimal
-   devuelve la representación en formato decimal con el formato RRRGGGBB*/
-
-
-
-function converter(){
+function convertir(){
                    
-    var hex='#';
-    var h=document.getElementById('rgb').value;
+  var hexadecimal='#';
+  var h=document.getElementById('rgb').value;
 
 if(h.length==4){
-    for(var i=1;i < h.length;i++){
-        hex+=h[i]+h[i];
-    }
+  for(var i=1;i < h.length;i++){
+      hex+=h[i]+h[i];
+  }
 }
 else if(h.length==7){
-    hex=h;
+  hex=h;
 }
 else
-    {
-      window.alert("No es valido");
-    }
+  {
+    window.alert("No es valido");
+  }
 var getHex=hex.substring(1);
 var getHex1=getHex.substring(0,2);
 var getHex2=getHex.substring(2,4);
@@ -51,8 +44,64 @@ var getInt1=parseInt(getHex1,16);
 var getInt2=parseInt(getHex2,16);
 var getInt3=parseInt(getHex3,16);
 var setInt = document.getElementById('int')
- setInt.value="R: "+getInt1+"        G: "+getInt2+"      B: " + getInt3;
-  };
+setInt.value="R: "+getInt1+"       G: "+getInt2+"     B: " + getInt3;
+};
+
+
+function rgb(r, g, b){
+    r=rango(r);
+    g=rango(g);
+    b=rango(b);
+  var hexa;
+  hexa="#"+(convertAHexa(r)+convertAHexa(g)+convertAHexa(b)).toUpperCase();
+  return hexa;
+}
+
+function convertAHexa(x){
+var h;
+h=parseInt(x).toString(16);
+return h.length == 1 ? "0" + h : h;
+}
+
+function rango(x){
+  if (x>255)x=255;
+  else if(x<0)x=0;
+  return x;
+}
+/* Función que al pasar un valor RGB en representación hexadecimal
+   devuelve la representación en formato decimal con el formato RRRGGGBB*/
+
+
+function hex(hex){
   
+  var r,g,b,i;
+    if (hex.substr(0,1)=='#'){
+    i=1; 
+  }
+  else {
+    i=2;
+  }
+  r=hex.substr(i, 2);
+  r=aDecimal(r);
+  r=concero(r);
+  g=hex.substr(i+2, 2);
+  g=aDecimal(g);
+  g=concero(g);
+  b=hex.substr(i+4, 2);
+  b=aDecimal(b);
+  b=concero(b);
+  return r+""+g+""+b;  
+}
+  
+  function aDecimal(hex){
+    var decimal=parseInt(hex,16);
+    return decimal;
+  }
+
+  function concero(x){
+    if (x<10)x="0"+x;
+    if (x<100)x="0"+x;
+    return x;
+  }
 
     
